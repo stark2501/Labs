8b11235ecebc462d5312484d970b4948d47fc013
stark2501 <44257897+stark2501@users.noreply.github.com>
2018-10-19 14:39:56 +0700

    Update Lab 02.cpp

---
+++ Lab 02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	cout <<endl;
+	cout <<"\t*   *" <<"\t*****" <<"\t*   *" <<"\t*   *" <<"\t*****" <<"\t*   *" <<endl;
+	cout <<"\t**  *" <<"\t*    " <<"\t*   *" <<"\t * * " <<"\t*    " <<"\t**  *" <<endl;
+	cout <<"\t* * *" <<"\t*  **" <<"\t*   *" <<"\t  *  " <<"\t*****" <<"\t* * *" <<endl;
+	cout <<"\t*  **" <<"\t*   *" <<"\t*   *" <<"\t  *  " <<"\t*    " <<"\t*  **" <<endl;
+	cout <<"\t*   *" <<"\t*****" <<"\t*****" <<"\t  *  " <<"\t*****" <<"\t*   *" <<endl;
+}
