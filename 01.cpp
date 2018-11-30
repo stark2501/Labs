fbe310918c0e6dbe384e47058d8f0185c5d1e19a
stark2501 <44257897+stark2501@users.noreply.github.com>
2018-11-30 14:10:40 +0700

    Lab 02

--- 01.cpp
+++ 01.cpp
@@ -1,8 +1,33 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+class Cphone
+{
+	private:
+		int areacode;
+		int exchange;
+		int number;
+		string mynumber;
+	public:
+		void nhap();
+		void xuat();
+};
+void Cphone::nhap()
+{
+	mynumber="(212) 767-8900";
+	cout<<"Enter your area code, exchange and number: ";
+	cin>>areacode;cin>>exchange;cin>>number;
+}
+void Cphone::xuat()
+{
+	cout<<"My number is: "<<mynumber<<endl;
+	cout<<"Your number is: "<<"("<<areacode<<")"<<" "<<exchange<<" "<<number<<endl;
+}
 int main()
 {
-	cout <<"HELLO WORLD";
+	Cphone h;
+	h.nhap();
+	h.xuat();
 	return 0;
 }
--- 02.cpp
+++ 02.cpp
@@ -1,13 +1,26 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main()
 {
-	cout <<endl;
-	cout <<"\t*   *" <<"\t*****" <<"\t*   *" <<"\t*   *" <<"\t*****" <<"\t*   *" <<endl;
-	cout <<"\t**  *" <<"\t*    " <<"\t*   *" <<"\t * * " <<"\t*    " <<"\t**  *" <<endl;
-	cout <<"\t* * *" <<"\t*  **" <<"\t*   *" <<"\t  *  " <<"\t*****" <<"\t* * *" <<endl;
-	cout <<"\t*  **" <<"\t*   *" <<"\t*   *" <<"\t  *  " <<"\t*    " <<"\t*  **" <<endl;
-	cout <<"\t*   *" <<"\t*****" <<"\t*****" <<"\t  *  " <<"\t*****" <<"\t*   *" <<endl;
+	int a,b;
+	float c;
+	cout<<"Enter initial amount: "<<endl;
+	cin>>a;
+	cout<<"Enter number of years: "<<endl;
+	cin>>b;
+	cout<<"Enter interest rate (percent per year): "<<endl;
+	cin>>c;
+	double sum=0;
+	for(int i=1;i<=b;i++)
+	{
+		sum=a+(a*(c/100));
+		a=sum;
+	}
+	cout<<sum;
+	return 0;
 }
+
+		
